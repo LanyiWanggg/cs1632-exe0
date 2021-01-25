c277a4dd7fa920e0763487f5c638fd22a15a8781
Lanyi Wang <lanyiwang@Lanyis-MacBook-Pro.local>
2021-01-24 19:24:06 -0500

    first commit

---
+++ SortedCollection.java
@@ -0,0 +1,68 @@
+//TODO: Import libraries as needed
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Collections;
+
+public class SortedCollection {
+	// TODO: Add member variables or methods as needed
+	ArrayList <Integer> list;
+	public SortedCollection(){
+		list = new ArrayList<>();
+	}
+
+	/**
+	 * Adds the number n to the collection.
+	 * 
+	 * @param n the number to add to the collection
+	 * @return always returns true
+	 */
+	public boolean add(int n) {
+		// TODO: Implement
+		list.add(n);
+		return true;
+	}
+
+	/**
+	 * Removes the smallest number in the collection and returns it.
+	 * 
+	 * @return the smallest number in the collection
+	 */
+	public int remove() throws NoSuchElementException {
+		// TODO: Implement
+		return list.remove(list.indexOf(Collections.min(list)));
+	}
+
+	/**
+	 * Prints usage information.
+	 */
+	public static void showUsage() {
+		System.out.println("Usage: java SortedCollection [num1] [num2] [num3] ...");
+	}
+
+	/**
+	 * Main method. Receives a list of numbers as commandline arguments and prints
+	 * out the list in sorted order from smallest to largest.
+	 * 
+	 * @param args commandline arguments; see showUsage() for detailed information
+	 */
+	public static void main(String[] args) {
+		SortedCollection collection = new SortedCollection();
+		if (args.length == 0) {
+			showUsage();
+			return;
+		}
+		
+		// TODO: add numbers in commandline arguments to collection using the add(int) method.
+		// If any commandline argument is not a number, call showUsage() and return.
+		for (int i = 0; i<args.length;i++){
+			collection.add(Integer.parseInt(args[i]));
+		}
+		
+		System.out.print("sorted: ");
+		for (int i = 0; i < args.length; i++) {
+			int num = collection.remove();
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+}
